70482d724163b257c8ad350d0cd11a2f9c6aaaba
Zhiyong Guo <guozy0103@outlook.com>
2023-06-18 20:20:07 -0400

    fix: remove uselss props of 25572

--- scripts/25572.jsx
+++ scripts/25572.jsx
@@ -3,7 +3,6 @@ import { createContext, useContext, memo } from "react";
 import get from "lodash/get";
 import Typography from "@mui/material/Typography";
 import { useRecordContext } from "ra-core";
-import { sanitizeFieldRestProps } from "./sanitizeFieldRestProps";
 
 export const RecordContext = createContext(undefined);
 
@@ -18,25 +17,22 @@ export const useRecordContext = (props) => {
   return (props && props.record) || context;
 };
 
-export const RichTextField =
-  memo <
-  RichTextFieldProps >
-  ((props) => {
-    const { className, emptyText, source, stripTags = false, ...rest } = props;
-    const record = useRecordContext(props);
-    const value = get(record, source);
-    return (
-      <Typography className={className} variant="body2" component="span">
-        {value == null && emptyText ? (
-          emptyText
-        ) : stripTags ? (
-          removeTags(value)
-        ) : (
-          <span dangerouslySetInnerHTML={{ __html: value }} />
-        )}
-      </Typography>
-    );
-  });
-
 export const removeTags = (input) =>
   input ? input.replace(/<[^>]+>/gm, "") : "";
+
+export const RichTextField = memo((props) => {
+  const { className, emptyText, source, stripTags = false } = props;
+  const record = useRecordContext(props);
+  const value = get(record, source);
+  return (
+    <Typography className={className} variant="body2" component="span">
+      {value == null && emptyText ? (
+        emptyText
+      ) : stripTags ? (
+        removeTags(value)
+      ) : (
+        <span dangerouslySetInnerHTML={{ __html: value }} />
+      )}
+    </Typography>
+  );
+});
