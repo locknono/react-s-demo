2ca8996b4150cf4b24c1d1c9a555305b3610db77
Zhiyong Guo <guozy0103@outlook.com>
2023-06-21 23:55:24 -0400

    feat: cve 25572

--- scripts/25572.jsx
+++ scripts/25572.jsx
@@ -1,33 +1,28 @@
 import * as React from "react";
-import { createContext, useContext, memo } from "react";
+import { createContext, useContext, useState, memo } from "react";
 import get from "lodash/get";
 import Typography from "@mui/material/Typography";
-import { useRecordContext } from "ra-core";
-
-export const RecordContext = createContext(undefined);
-
-export const RecordContextProvider = ({ children, value }) => (
-  <RecordContext.Provider value={value}>{children}</RecordContext.Provider>
-);
 
 export const useRecordContext = (props) => {
   // Can't find a way to specify the RecordType when CreateContext is declared
-  // @ts-ignore
-  const context = useContext < RecordType > RecordContext;
+  const context = useContext(RecordContext);
   return (props && props.record) || context;
 };
 
 export const removeTags = (input) =>
   input ? input.replace(/<[^>]+>/gm, "") : "";
 
+export const RecordContext = createContext(undefined);
+
 export const RichTextField = memo((props) => {
   const { className, emptyText, source, stripTags = false } = props;
+  const [text, setText] = useState("");
   const record = useRecordContext(props);
   const value = get(record, source);
   return (
     <Typography className={className} variant="body2" component="span">
       {value == null && emptyText ? (
-        emptyText
+        text
       ) : stripTags ? (
         removeTags(value)
       ) : (
